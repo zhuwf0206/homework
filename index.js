70b92652389e67fa1df4cc112fd33569a30e86c3
zhuifeng <490111522@qq.com>
2020-06-21 13:30:44 +0800

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 * @Author: anchen
 * @Date:   2020-06-21 13:18:52
 * @Last Modified by:   anchen
-* @Last Modified time: 2020-06-21 13:25:17
+* @Last Modified time: 2020-06-21 13:28:58
 */
 
 $(document).ready(function(){
-    
+    alert("hello");
 });
