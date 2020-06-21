7fe2ea65e61e6c67587089b4b5bba384c190a988
zhuifeng <490111522@qq.com>
2020-06-21 13:37:44 +0800

    another change

---
+++ dir2/app2.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: anchen
+* @Date:   2020-06-21 13:35:44
+* @Last Modified by:   anchen
+* @Last Modified time: 2020-06-21 13:36:36
+*/
+
+$(document).ready(function(){
+    alert("222222")
+});
